cc2bd2b4ab5d0861dbc619ee841d30707d22462c
Nader PJR <pjrnader@gmail.com>
2026-06-05 11:29:50 +0430

    introduction to microservices with moleculer microservices framework

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import UserService from './services/user.service.js';
+import EmailService from './services/email.service.js';
+import AuthService from './services/auth.service.js';
 
 async function startApp() {
     await UserService.start();
+    await EmailService.start();
+    await AuthService.start();
+
     try {
         //simulate user creation
         const newUser = await UserService.call('user.createUser', {
@@ -11,10 +16,32 @@ async function startApp() {
         console.log('New User created: ', newUser);
         const users = await UserService.call('user.getUsers');
         console.log('All users: ', users);
+
+        // simulate sending email 
+
+        const emailResult = await EmailService.call('email.sendEmail', {
+            recipient: newUser.email,
+            subject: 'welcome to our platform',
+            content: 'Thank u for signing up.',
+        });
+
+        console.log(emailResult);
+
+        // simulate auth 
+
+        const authResult = await AuthService.call('auth.authUser', {
+            username: 'admin',
+            password: 'password'
+        })
+
+        console.log('Auth result: ', authResult)
+
     } catch (error) {
         console.log('Error: ', error)
     } finally {
         await UserService.stop();
+        await EmailService.stop();
+        await AuthService.stop();
     }
 }
 
